33907f3e23ccb05e9ba2e3e462363f31e31fb238
wgreunke <wgreunke@gmail.com>
2024-04-01 15:07:46 -0700

    Got it workgin with Wikipedia

--- test_tools.py
+++ test_tools.py
@@ -1,4 +1,18 @@
 #This file test the LangChain Wikipedia Connector
+print("")
+print("********************")
 
+
+#Connect to Wikipedia
+from langchain_community.tools import WikipediaQueryRun
+from langchain_community.utilities import WikipediaAPIWrapper
+
+
+api_wrapper = WikipediaAPIWrapper(top_k_results=1, doc_content_chars_max=200)
+wiki_tool = WikipediaQueryRun(api_wrapper=api_wrapper)
+
+#print(wiki_tool.run({"query": "Volkswagon Bus"}))
+
+#Connect to Yahoo Finance
 
 
